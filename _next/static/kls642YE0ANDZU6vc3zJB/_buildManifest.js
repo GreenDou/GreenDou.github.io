86cd922b1ef74541853031c19040ecdcdabe8ef3
Greendou He <greendou@MacBook-Pro.local>
2022-07-13 23:25:37 +0800

    update

---
+++ _next/static/kls642YE0ANDZU6vc3zJB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c617ef6d0552f8b8.js"],"/_error":["static/chunks/pages/_error-5a04a9efe27215f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
