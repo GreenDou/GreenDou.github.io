d58db4ab48e4e8df99077f8ae354c2b052e05217
GreenDou <zn.he@foxmail.com>
2022-07-23 11:20:43 +0000

    Deploying to master from @ GreenDou/genshin-artifact@a6673cb6088e7b033183bfc199df59bd5d59b92b 🚀

--- _next/static/VkyvNKhlSdO1Nt_swtmlh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8979df148049ae12.js"],"/_error":["static/chunks/pages/_error-266f3102f711161d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
